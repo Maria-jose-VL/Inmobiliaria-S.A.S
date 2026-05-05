596de9d34be0bbe3a45f2935286bb74b177dda1b
Ju4an20031 <juane.rodriguezt@uqvirtual.edu.co>
2026-05-05 16:56:18 -0500

    a

---
+++ Muebles.ex
@@ -0,0 +1,19 @@
+defmodule Muebles do
+  @moduledoc """
+  Muebles keeps the contexts that define your domain
+  and business logic.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Muebles.hello()
+      :world
+
+  """
+  def hell do
+    :world
+  end
+end
